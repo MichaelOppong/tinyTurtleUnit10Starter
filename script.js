2463b3c6005af1beaeada2ce5e6a6b35be623d9d
Michael Oppong <michael@scripted.org>
2017-03-06 18:31:57 +0000

    solution code u10p1

--- script.js
+++ script.js
@@ -11,8 +11,52 @@ forward(50);
 right(90);
 forward(50);
 }
+ 
+function rectangle(){
+forward(50);
+right(90);
+forward(90);
+right(90);
+forward(50);
+right(90);
+forward(90);
+}
+
+function triangle(x){
+right(60);
+forward(x);
+right(120);
+forward(x);
+right(120);
+forward(x);
+}
+
+function house(){
+forward(50);
+right(90);
+forward(50);
+right(90);
+forward(50);
+right(90);
+forward(50);
+right(90);
+forward(50);
+right(60);
+forward(25);
+right(55);
+forward(30);
+
+}
+triangle(70);
+
+triangle(100);
+
+triangle(200);
 
 // Type your function call below
-
-
+/*square();
+rectangle();
+triangle();
+house();
+*/
 stamp();
